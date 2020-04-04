84518695f30322ce72df8f2affba42c9f4763137
deradev <de ra>
2020-04-04 15:42:11 +0200

    Updated build script for cumulative builds.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 
 # Go build outside of GOPATH requires modular build from current working directory
 
-cd src/quic-go 
+cd src/quic-go
 go build ./...
 
 cd ../../src/dash/caddy
@@ -18,4 +18,4 @@ go build -o proxy_module.so -buildmode=c-shared proxy_module.go
 cd ../../../../
 mv src/dash/client/proxy_module/proxy_module.h src/AStream/dist/client/
 mv src/dash/client/proxy_module/proxy_module.so src/AStream/dist/client/
-mv src/dash/client/proxy_module/conn.py src/AStream/dist/client/
+cp src/dash/client/proxy_module/conn.py src/AStream/dist/client/
