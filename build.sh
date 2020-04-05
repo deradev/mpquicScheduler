bf1917bc8dc16d74553b43e3b6103143814986e6
deradev <de ra>
2020-04-05 13:04:35 +0200

    Include proxy module header in sources.

--- build.sh
+++ build.sh
@@ -16,6 +16,6 @@ go build -o proxy_module.so -buildmode=c-shared proxy_module.go
 # Copy python-go code bridge requirements
 
 cd ../../../../
-mv src/dash/client/proxy_module/proxy_module.h src/AStream/dist/client/
+cp src/dash/client/proxy_module/proxy_module.h src/AStream/dist/client/
 mv src/dash/client/proxy_module/proxy_module.so src/AStream/dist/client/
 cp src/dash/client/proxy_module/conn.py src/AStream/dist/client/
